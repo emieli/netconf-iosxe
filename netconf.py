09de23c08fd131f501c7ccf9f14ec34e6e24e631
emil <emil.eliason@gmail.com>
2022-09-08 14:03:05 -0500

    configure loopback

--- netconf.py
+++ netconf.py
@@ -3,7 +3,35 @@ from ncclient import manager
 from rich import print
 from lxml import etree
 
-if __name__ == "__main__":
+def get_capabilities(device):
+    with manager.connect(**device) as nconf:
+        print(list(nconf.server_capabilities)) 
+
+def get_config(device):
+
+    with manager.connect(**device) as nconf:
+        nc_reply = nconf.get_config(source="running")
+        xml_data = etree.tostring(
+            nc_reply.data_ele, 
+            pretty_print=True
+        ).decode()
+        print(xml_data)
+    return
+
+def configure_loopback(device):
+
+    with open('loopback_config.xml', 'r') as file:
+        loopback_config = file.read()
+    
+    with manager.connect(**device) as m:
+        nc_reply = m.edit_config(target='candidate', config=loopback_config)
+        print(nc_reply)
+        nc_reply = m.commit()
+        print(nc_reply)
+    
+    return
+
+def main():
 
     device = {
         "host": "10.0.0.2",
@@ -12,18 +40,11 @@ if __name__ == "__main__":
         "password": "emil",
         "hostkey_verify": False,
     }
-    # with manager.connect(**device) as nconf:
-    #     print(list(nconf.server_capabilities)) 
 
-    '''imports and "device" definition omitted '''
-    # with manager.connect(**device) as nconf:
-    #     nc_reply = nconf.get_config(source="running")
-    #     print(type(nc_reply)) 
+    #get_capabilities(device)
+    #get_config(device)
+    configure_loopback(device)
 
-    with manager.connect(**device) as nconf:
-        nc_reply = nconf.get_config(source="running")
-        xml_data = etree.tostring(
-            nc_reply.data_ele, 
-            pretty_print=True
-        ).decode()
-        print(xml_data) 
+
+if __name__ == "__main__":
+    main()
